356178f5d56b519df3368714677307a81df5e16a
Andrey <amigin@gmail.com>
2021-05-11 10:30:23 +0300

    exposed Blobs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
-pub mod my_azure_storage;
+mod my_azure_storage;
 
 pub use my_azure_storage::blobs::block_blob::BlockBlob;
+pub use my_azure_storage::blobs::page_blob::PageBlob;
+pub use my_azure_storage::AzureConnection;
