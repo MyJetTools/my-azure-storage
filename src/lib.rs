30a80800af5b3cbc9a9d95369142b9b41da242ce
Andrey <amigin@gmail.com>
2021-05-14 16:29:52 +0300

    Added AzureStorageError;

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub use my_azure_storage::blobs::block_blob::BlockBlob;
 pub use my_azure_storage::blobs::page_blob::PageBlob;
 pub use my_azure_storage::blobs::page_blob::BLOB_PAGE_SIZE;
 pub use my_azure_storage::AzureConnection;
+pub use my_azure_storage::AzureStorageError;
