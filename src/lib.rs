cac6ac1e96e9df5543b152fb888d9eb0f8b82fbb
Andrey <amigin@gmail.com>
2021-06-04 10:03:43 +0300

    Exposed blobs namespace

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
-mod my_azure_storage;
+pub mod my_azure_storage;
 
+pub use my_azure_storage::AzureConnection;
+pub use my_azure_storage::AzureStorageError;
+
+/*
 pub use my_azure_storage::blobs::block_blob::BlockBlob;
 pub use my_azure_storage::blobs::models::BlobProperties;
 pub use my_azure_storage::blobs::page_blob::PageBlob;
 pub use my_azure_storage::blobs::page_blob::BLOB_PAGE_SIZE;
-pub use my_azure_storage::AzureConnection;
-pub use my_azure_storage::AzureStorageError;
+
+ */
