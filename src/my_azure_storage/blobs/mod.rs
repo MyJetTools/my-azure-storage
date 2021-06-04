632bedb25a8d25ce5dfb8f52a8e168eb56acb6cd
Andrey <amigin@gmail.com>
2021-06-04 10:06:54 +0300

    Updated Strcuture

--- src/my_azure_storage/blobs/mod.rs
+++ src/my_azure_storage/blobs/mod.rs
@@ -2,5 +2,8 @@ mod blob_models;
 pub mod block_blob;
 pub mod container_models;
 pub mod containers;
-pub mod models;
+mod models;
 pub mod page_blob;
+
+pub use models::AzureItems;
+pub use models::BlobProperties;
