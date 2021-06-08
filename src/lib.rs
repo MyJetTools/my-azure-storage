8ae453d525ef4096b818b1943f5789eb501f7c2f
Andrey <amigin@gmail.com>
2021-06-08 15:58:49 +0300

    exposed BLOB_PAGE_SIZE

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,5 @@ pub use my_azure_storage::blob;
 pub use my_azure_storage::blob_container;
 pub use my_azure_storage::block_blob;
 pub use my_azure_storage::page_blob;
+
+pub use my_azure_storage::page_blob::BLOB_PAGE_SIZE;
