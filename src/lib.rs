f15e9088e0a14323b69d2850fff474f586c8df9c
Andrey <amigin@gmail.com>
2021-11-19 15:06:29 +0200

    exposed Azure connection

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,14 @@ mod azure_response_handler;
 pub mod blob;
 pub mod blob_container;
 pub mod block_blob;
-pub mod connection;
+mod connection;
 mod consts;
 mod fl_requests;
 mod flurl_ext;
 pub mod page_blob;
 mod sign_utils;
 mod types;
+
+pub use connection::{
+    AzureConnection, AzureConnectionInfo, AzureConnectionWithTelemetry, GetAzureConnectionInfo,
+};
